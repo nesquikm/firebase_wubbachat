eee5fd7ff7c69428a231120c6ecc8afe80c9a8ca
nesquikm <drapolyuk@gmail.com>
2022-06-15 13:06:02 +0400

    First message sent and received

---
+++ README.md
@@ -0,0 +1,18 @@
+# Firebase Wubbachat
+
+A simple chat that uses Firebase Cloud Messages and Firebase Cloud Functions.
+
+---
+
+
+## Starting emulator
+
+```sh
+firebase emulators:start
+```
+
+## Deploy to a production environment
+
+```sh
+firebase deploy --only functions
+```
--- functions/index.js
+++ functions/index.js
@@ -30,53 +30,59 @@ exports.sendMessage = functions.https.onCall((data, context) => {
   };
 
   admin
-      .messaging()
-      .send(payload)
-      .then((response) => {
+    .messaging()
+    .send(payload)
+    .then((response) => {
       // Response is a message ID string.
-        console.log("Successfully sent message:", response);
-        return {success: true};
-      })
-      .catch((error) => {
-        return {error: error.code};
-      });
+      console.log("Successfully sent message:", response);
+      return { success: true };
+    })
+    .catch((error) => {
+      return { error: error.code };
+    });
 
   return {
     altered: `Response for ${data.text}`,
   };
 });
 
-
 exports.sendMessageToTopic = functions.https.onCall((data, context) => {
   functions.logger.info("========== DATA ===========", {
     structuredData: true,
   });
 
-  const message = data.text;
-  const topic = data.topic;
+  const { id, topic, messageBody, fromId, fromNickname } = data;
+
+  const expTopic = /([0-9a-zA-Z-]+)_(.*)/;
+  const match = topic.match(expTopic);
+  const chatName = decodeURI(match[2]);
 
   const payload = {
     topic: topic,
     notification: {
-      title: "cloud function demo",
-      body: message,
+      // TODO(nesquikm): parse data to get title
+      title: chatName,
+      body: messageBody,
     },
     data: {
-      body: message,
+      id,
+      fromId,
+      fromNickname,
+      messageBody: messageBody,
     },
   };
 
   admin
-      .messaging()
-      .send(payload)
-      .then((response) => {
+    .messaging()
+    .send(payload)
+    .then((response) => {
       // Response is a message ID string.
-        console.log("Successfully sent message:", response);
-        return {success: true};
-      })
-      .catch((error) => {
-        return {error: error.code};
-      });
+      console.log("Successfully sent message:", response);
+      return { success: true };
+    })
+    .catch((error) => {
+      return { error: error.code };
+    });
 
   return {
     altered: `Response for ${data.text}`,
