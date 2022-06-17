a725ada214683c0d25340b8ea3000e404708b7c4
nesquikm <drapolyuk@gmail.com>
2022-06-17 16:16:14 +0400

    Removed legacy, added user color

--- functions/index.js
+++ functions/index.js
@@ -4,54 +4,13 @@ admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-exports.sendMessage = functions.https.onCall((data, context) => {
-  functions.logger.info("========== DATA ===========", {
-    structuredData: true,
-  });
-
-  const message = data.text;
-  const FCMToken = data.token;
-
-  const payload = {
-    token: FCMToken,
-    notification: {
-      title: "cloud function demo",
-      body: message,
-    },
-    data: {
-      body: message,
-    },
-  };
-
-  admin
-    .messaging()
-    .send(payload)
-    .then((response) => {
-      // Response is a message ID string.
-      console.log("Successfully sent message:", response);
-      return { success: true };
-    })
-    .catch((error) => {
-      return { error: error.code };
-    });
-
-  return {
-    altered: `Response for ${data.text}`,
-  };
-});
 
 exports.sendMessageToTopic = functions.https.onCall((data, context) => {
   functions.logger.info("========== DATA ===========", {
     structuredData: true,
   });
 
-  const { id, topic, messageBody, fromId, fromNickname } = data;
+  const { id, topic, messageBody, fromId, fromNickname, fromColor } = data;
 
   const expTopic = /([0-9a-zA-Z-]+)_(.*)/;
   const match = topic.match(expTopic);
@@ -68,6 +27,7 @@ exports.sendMessageToTopic = functions.https.onCall((data, context) => {
       id,
       fromId,
       fromNickname,
+      fromColor,
       messageBody: messageBody,
     },
   };
